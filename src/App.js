2be1752c3a94a4fde491e4288ca73d4629f305a9
Trinidad Boragini <mtboragini@gmail.com>
2021-09-27 14:47:15 -0300

    Prevent duplicates

--- src/App.js
+++ src/App.js
@@ -12,13 +12,10 @@ import withReactContent from "sweetalert2-react-content";
 library.add(fas);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor ( props ) {
+    super( props );
     this.state = {
-      todoList: [
-        // { id: 0, description: "Task one" },
-        // { id: 1, description: "Task two" },
-      ],
+      todoList: [],
       currentTodo: {
         id: 0,
         description: "",
@@ -27,57 +24,76 @@ class App extends Component {
   }
 
   getMaxId = () => {
-    if (this.state.todoList.length) {
-      const max = Math.max(...this.state.todoList.map((el) => el.id));
+    if ( this.state.todoList.length ) {
+      const max = Math.max( ...this.state.todoList.map( ( el ) => el.id ) );
       return max;
     } else {
       return -1;
     }
   };
 
-  handleChange = (event) => {
+  handleChange = ( event ) => {
     const maxId = this.getMaxId();
-    this.setState({
+    this.setState( {
       currentTodo: {
         id: maxId + 1,
         description: event.target.value.trim(),
       },
-    });
+    } );
   };
 
   clearField() {
-    document.getElementById("todo-form").reset();
+    document.getElementById( "todo-form" ).reset();
   }
 
-  successAlert = (title) => {
-    const MySwal = withReactContent(Swal);
-    return MySwal.fire({
+  showAlert = ( title, icon ) => {
+    const MySwal = withReactContent( Swal );
+    return MySwal.fire( {
       position: "center",
-      icon: "success",
+      icon: icon,
       title: title,
       showConfirmButton: false,
       timer: 1200,
       width: "300px",
-    });
+    } );
   };
 
   getDescriptions = () => {
-    const descriptions = this.state.todoList.map((el) =>
+    const descriptions = this.state.todoList.map( ( el ) =>
       el.description.toLowerCase()
     );
     return descriptions;
   };
 
+  checkDuplicated = ( description ) => {
+    const descriptions = this.getDescriptions();
+    const descriptionToLower = description.trim().toLowerCase();
+
+    return descriptions.includes( descriptionToLower );
+  };
+
   addNewTodo = ( event ) => {
     event.preventDefault();
     const newTodo = this.state.currentTodo;
-    if (newTodo.description !== "") {
-      this.setState({
-        todoList: [...this.state.todoList, newTodo],
-      });
-      this.successAlert("Your task has been submitted!");
-      this.clearField();
-    }
+    const isDuplicated = this.checkDuplicated( newTodo.description );
+
+    if ( newTodo.description === "" || newTodo.description === /\s/ ) {
+      this.showAlert( "You must write a task!", "warning" );
+    } else {
+      if ( isDuplicated ) {
+        this.showAlert( "This task is already submitted!", "error" )
+      } else {
+        this.setState( {
+          todoList: [ ...this.state.todoList, newTodo ],
+          currentTodo: {
+            id: 0,
+            description: "",
+          },
+        } );
+        this.showAlert( "Your task has been submitted!", "success" );
+      }
+    };
+    this.clearField();
   };
 
   deleteTodo = (id) => {
@@ -96,7 +112,7 @@ class App extends Component {
         this.setState({
           todoList: filteredTodos,
         });
-        this.successAlert("Your task has been deleted");
+        this.showAlert("Your task has been deleted", "success");
       }
     });
   };
@@ -109,10 +125,10 @@ class App extends Component {
         <div className="todo-container">
           <div className="todo-header">
             <TodoForm
-              currentTodo={ this.state.currentTodo }
-              addNewTodo={ this.addNewTodo }
-              handleChange={ this.handleChange }
-              getDescriptions={ this.getDescriptions}
+              currentTodo={this.state.currentTodo}
+              addNewTodo={this.addNewTodo}
+              handleChange={this.handleChange}
+              getDescriptions={this.getDescriptions}
             />
           </div>
           <div className="todo-body">
